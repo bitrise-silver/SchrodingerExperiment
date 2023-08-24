420d6a1cb7100a6683d4962db2beba2bfb3fb5c8
Silvercast <silvercast.nguyen@bitrise.io>
2023-08-24 12:48:21 +0200

    Initial Commit

---
+++ SchrodingerExperiment/SchrodingerExperimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchrodingerExperimentApp.swift
+//  SchrodingerExperiment
+//
+//  Created by Silvercast Nguyen on 2023-08-24.
+//
+
+import SwiftUI
+
+@main
+struct SchrodingerExperimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
